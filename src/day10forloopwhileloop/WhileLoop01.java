c105564c3030ba0a2532397f195069ef48d9241e
Suleyman Alptekin <slptkn@yahoo.com>
2022-02-12 16:29:51 -0500

    While Loop

--- src/day10forloopwhileloop/WhileLoop01.java
+++ src/day10forloopwhileloop/WhileLoop01.java
@@ -58,19 +58,15 @@ public class WhileLoop01 {
         System.out.println(sumOfDigits);
 
 	 /*
-		 Type java code by using while loop,
+		 5.Example: Type java code by using while loop,
 	     Write a program that prompts the user to input a number.
 	     It should then print the multiplication table of that number.
 	     3x1=3  3x2=6  3x3=9  3x4=12 3x5=15  3x6=18  3x7=21  3x8=24  3x9=27  3x10=30
 	 */
      int n = 3;
-
      int m = 1;
-
      while(m<11){
-
          System.out.println(n + "x" + m + "=" + n*m);
-
          m++;
      }
     }
