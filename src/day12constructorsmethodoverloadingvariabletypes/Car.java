dbeabd9e9a088ef282b3d3ff751248655e9b0885
Suleyman Alptekin <slptkn@yahoo.com>
2022-02-15 22:03:13 -0500

    Method Overloading

---
+++ src/day12constructorsmethodoverloadingvariabletypes/Car.java
@@ -0,0 +1,70 @@
+package day12constructorsmethodoverloadingvariabletypes;
+
+public class Car {
+
+    /*
+        1)Constructors are used to create object from a class
+        2)Java puts default constructors inside every class
+        3)Default constructors does not have any parameters inside the constructor parenthesis
+        4)Constructor names must match with the class names
+        5)In a class we can have multiple constructors
+
+        How to create a constructor?
+        1)Type access modifier
+        2)TYpe the class name as constructor name
+        3)Open-close parenthesis
+        4)Open curly brace press enter
+
+        What are the differences between "constructors" and "methods"?
+        1)Constructors do not have return type but methods must have
+        2)Constructor names must be same with the class name because of that you cannot decide constructor names
+          but method names can be assigned by developer.
+    */
+    String make;
+    String model;
+    double price;
+    int year;
+    //That constructor is same with the default constructor, it is very simple
+    public Car(){
+
+    }
+
+    public Car(String make, String model, double price, int year){
+        this.make = make;
+        this.model = model;
+        this.price = price;
+        this.year = year;
+    }
+
+    public Car(String make, String model){
+        this.make = make;
+        this.model = model;
+    }
+
+    public static void main(String[] args) {
+
+        Car c1 = new Car();
+        c1.make="Honda";
+        c1.model="Civic";
+        c1.price=10000;
+        c1.year=2015;
+
+        Car c2 = new Car();
+        c2.make="Audi";
+        c2.model="S5";
+        c2.price=20000;
+        c2.year=2019;
+
+        Car c3 = new Car("BMW", "M4", 70000, 2019);
+
+        Car c4 = new Car("Mercedes", "GLE", 80000, 2021);
+
+        Car c5 = new Car("Tofas", "Sahin ELXIV");
+    }
+
+    public String run(){
+        return "I started to run";
+    }
+
+
+}
---
+++ src/day12constructorsmethodoverloadingvariabletypes/MethodOverloading01.java
@@ -0,0 +1,57 @@
+package day12constructorsmethodoverloadingvariabletypes;
+
+public class MethodOverloading01 {
+
+    /*
+        1)If you have multiple methods whose names are same, Java looks at the parameters to select
+          a specific method.
+          a)To select number of parameters are important
+          b)To select data types of parameters are important
+
+        2)If you have multiple methods with the same name in a class it is called "Method Overloading"
+          How can you achieve method overloading?
+          i)Create methods with the same name
+          ii)Use the different parameters
+             a)Change the number of parameters
+             b)Change the data types of the parameters
+             c)If the data types are different, you can change their places
+     */
+
+    public static void main(String[] args) {
+        //add(3, 5) is called "Method Call" in Java
+        System.out.println(add(3, 5));
+        System.out.println(add(7, 4));
+
+        System.out.println(add(1, 4, 2));
+
+        System.out.println(add(1.2, 2.3));
+
+        System.out.println(add(4, 3.4));
+
+    }
+    //Create an add method which adds 2 integers
+    //If you use a method inside the main method use "static" keyword between the access modifier and the "return type"
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    //Create an add method which adds 3 integers
+    public static int add(int a, int b, int c){
+        return a+b+c;
+    }
+    //Create an add method which adds 2 doubles
+    public static double add(double a, double b){
+        return a+b;
+    }
+
+    //Create an add method which adds 1 integer and 1 double
+    public static double add(int a, double b){
+        return a+b;
+    }
+
+    //Create an add method which adds 1 double and 1 integer
+    public static double add(double a, int b){
+        return a+b;
+    }
+
+}
