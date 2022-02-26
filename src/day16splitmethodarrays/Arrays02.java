11e6dabd02beb3708d04c3e1556641e95eef946a
Suleyman Alptekin <slptkn@yahoo.com>
2022-02-26 09:43:15 -0500

    Arrays

---
+++ src/day16splitmethodarrays/Arrays02.java
@@ -0,0 +1,18 @@
+package day16splitmethodarrays;
+
+import java.util.Arrays;
+
+public class Arrays02 {
+    public static void main(String[] args) {
+
+        //Please split this one into its letters
+        String name = "FatmaSeher"; //[F,a,t,m,a,S,e,h,e,r]
+        String nameArray [] = name.split(""); //String
+        System.out.println(Arrays.toString(nameArray));
+
+        //Please split this name into a char array.
+        char array [] = name.toCharArray();//char
+        System.out.println("Char array => " + Arrays.toString(array));
+
+    }
+}
